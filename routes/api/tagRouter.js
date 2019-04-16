c6766b31d7bed9449297bf5ff107561fc0352210
bjsmith3185 <bjsmith3185@hotmail.com>
2019-04-16 12:22:33 -0400

    added conform function to input for new tag

--- routes/api/tagRouter.js
+++ routes/api/tagRouter.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 const check = require("../../middleware/jsonWebToken");
 const newTag = require('../../middleware/newTag');
 const searchTag = require('../../middleware/tagSearch');
-const validateNewTag = require('../../validate/validateNewTag')
+const validateNewTag = require('../../validate/validateNewTag');
+const conform = require('../../validate/conformInput')
 
 // Matches with "/api/tag"
 
@@ -19,25 +20,26 @@ router.route("/new").post(check.validateToken, (req, res) => {
         res.status(403).json({err: 'token not verified'})
       } else {
 
-        // console.log(req.body)
+        console.log(req.body)
+        req.body = conform.conformNewTagData(req.body)
+        console.log(req.body)
+      // let { errors, isValid } = validateNewTag(req.body);
+      // if (!isValid) {
+      //   console.log(errors)
+      //   return res.status(400).json(errors);
+      // }
 
-      let { errors, isValid } = validateNewTag(req.body);
-      if (!isValid) {
-        console.log(errors)
-        return res.status(400).json(errors);
-      }
+      // // console.log(req.body)
+      // console.log('success')
 
-      // console.log(req.body)
-      console.log('success')
-
-        newTag
-        .enterTag(req.body.state, req.body)
-        .then(dbresults => {
-          res.json(dbresults);
-        })
-        .catch(err => {
-          res.status(403).json({ err: err });
-        });
+      //   newTag
+      //   .enterTag(req.body.state, req.body)
+      //   .then(dbresults => {
+      //     res.json(dbresults);
+      //   })
+      //   .catch(err => {
+      //     res.status(403).json({ err: err });
+      //   });
 
         // res.json({
         //   text: "protected search route",
--- validate/conformInput.js
+++ validate/conformInput.js
@@ -1,7 +1,6 @@
 // this module will make sure the incoming data is in the correct format
 
 module.exports = {
-
   conformSearchData: function(value) {
     // Modify tag data if necessary
     let objCopy = Object.assign({}, value);
@@ -30,5 +29,29 @@ module.exports = {
     return objCopy;
   },
 
+  conformNewTagData: function(value) {
+    // Modify tag data if necessary
+    let objCopy = Object.assign({}, value);
+    // Modify tag
+    if (objCopy.tag.includes("-")) {
+      objCopy.tag = objCopy.tag.replace("-", "");
+    }
+    objCopy.tag = objCopy.tag.toLowerCase().trim();
+    // Modify state
+    objCopy.state = objCopy.state.toLowerCase().trim();
+    //Modify make
+    objCopy.vehicleMake = objCopy.vehicleMake.toLowerCase().trim();
+    //Modify model
+    objCopy.vehicleModel = objCopy.vehicleModel.toLowerCase().trim();
+    //Modify year
+    objCopy.vehicleYear = objCopy.vehicleYear.trim();
+    //Modify color
+    objCopy.vehicleColor = objCopy.vehicleColor.toLowerCase().trim();
+    //Modify owner
+    objCopy.owner = objCopy.owner.toLowerCase().trim();
+    //Modify address
+    objCopy.address = objCopy.address.toLowerCase().trim();
 
+    return objCopy;
+  }
 };
