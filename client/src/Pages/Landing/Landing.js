cd406b4e87437984f9d5dfc23df78b324d359903
bjsmith3185 <bjsmith3185@hotmail.com>
2019-05-02 14:46:01 -0400

    added style to the landing/login page.

--- client/src/Pages/Landing/Landing.js
+++ client/src/Pages/Landing/Landing.js
@@ -3,7 +3,6 @@ import "./Landing.css";
 // Redux
 import { connect } from "react-redux";
 
-
 class LandingPage extends Component {
   state = {
     userName: "",
@@ -18,7 +17,10 @@ class LandingPage extends Component {
     event.preventDefault();
     this.props.submitLogin(
       this.state.userName.toLowerCase().trim(),
-      this.state.myPassword.toString().toLowerCase().trim()
+      this.state.myPassword
+        .toString()
+        .toLowerCase()
+        .trim()
     );
     this.setState({
       userName: "",
@@ -36,52 +38,60 @@ class LandingPage extends Component {
   componentWillMount() {
     sessionStorage.clear();
   }
-  
 
   render = () => {
     // console.log(this.props)
-    const submitForm = (this.state.userName && this.state.myPassword)
+    const submitForm = this.state.userName && this.state.myPassword;
 
     return (
       <div className="landing-page-container">
-        <h1 className="landing-title text-center">Stop Data</h1>
+        <div className="landing-page-background"></div>
+        <div className="landing-page-formbox">
+          <h1 className="landing-title text-center">Info Stop</h1>
 
-        <div className="landing-demo text-center" onClick={this.demo}>
-          demo login
-        </div>
-
-        <div className="login-form text-center">
-          <div className="landing-input-area">
-            <div className="landing-label text-center">User Name</div>
-            <input
-              className="landing-input"
-              name="userName"
-              value={this.state.userName}
-              onChange={this.onChange}
-              placeholder="User Name"
-            />
+          <div className="landing-demo text-center" onClick={this.demo}>
+            demo login
           </div>
-          {this.props.errors.username && <div className='username-error'>{this.props.errors.username}</div>}
-    
-          <div className="landing-input-area">
-            <div className="landing-label text-center">Password</div>
-            <input
-              className="landing-input"
-              name="myPassword"
-              value={this.state.myPassword}
-              onChange={this.onChange}
-              placeholder="Password"
-            />
-            {this.props.errors.password && <div className='password-error'>{this.props.errors.password}</div>}
-           </div>
 
-          <button
-            className="landing-login-btn btn btn-info"
-            onClick={this.submit}
-            disabled={!submitForm}
-          >
-            Sign In
-          </button>
+          <div className="login-form text-center">
+            <div className="landing-input-area">
+              <div className="landing-label text-center">User Name</div>
+              <input
+                className="landing-input"
+                name="userName"
+                value={this.state.userName}
+                onChange={this.onChange}
+                placeholder="User Name"
+              />
+            </div>
+            {this.props.errors.username && (
+              <div className="username-error">{this.props.errors.username}</div>
+            )}
+
+            <div className="landing-input-area">
+              <div className="landing-label text-center">Password</div>
+              <input
+                className="landing-input"
+                name="myPassword"
+                value={this.state.myPassword}
+                onChange={this.onChange}
+                placeholder="Password"
+              />
+              {this.props.errors.password && (
+                <div className="password-error">
+                  {this.props.errors.password}
+                </div>
+              )}
+            </div>
+
+            <button
+              className="landing-login-btn btn btn-info"
+              onClick={this.submit}
+              disabled={!submitForm}
+            >
+              Sign In
+            </button>
+          </div>
         </div>
       </div>
     );
@@ -90,7 +100,7 @@ class LandingPage extends Component {
 
 // this brings in the state to display on this component
 const mapStateToProps = state => {
-// console.log(state)
+  // console.log(state)
   return {
     token: state.token,
     errors: state.errors
@@ -100,8 +110,8 @@ const mapStateToProps = state => {
 const mapDispachToProps = dispach => {
   return {
     submitLogin: (userName, password) => {
-      dispach({ type: "CHECK_PASSWORD", payload: { userName, password} });
-    },
+      dispach({ type: "CHECK_PASSWORD", payload: { userName, password } });
+    }
   };
 };
 
