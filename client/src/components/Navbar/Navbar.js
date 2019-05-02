2083b46c68ff862e79e6e343b4a18ef2ad5a29cf
bjsmith3185 <bjsmith3185@hotmail.com>
2019-05-02 19:49:56 -0400

    working on the navbar component. Added the name and info as well as a logout button. Need to finish a menu button for a dropdown menu

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,29 +1,53 @@
 import React, { Component } from "react";
 import "./Navbar.css";
-import history from '../../history/history'
+import history from "../../history/history";
 
 // Redux
 import { connect } from "react-redux";
 
 class Navbar extends Component {
-
-
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
   logout = () => {
-    this.props.logoutUser(this.props.userId)
-    history.push("/")
-  }
-  
+    this.props.logoutUser(this.props.userId);
+    history.push("/");
+  };
+
+  capitalize = string => {
+    return string
+      .toLowerCase()
+      .split(" ")
+      .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+      .join(" ");
+  };
 
   render() {
-  
     // console.log(this.props)
     return (
-      <div className='logout-area'>
-        <button onClick={this.logout}>Logout</button>
+      <div className="navbar-area">
+        <div className="navbar-dropdown">
+          <div className="navbar-menu">Menu</div>
+        </div>
+
+        <div className="navbar-center-div text-center">
+          <div className="navbar-appname text-center">Know Your Stop</div>
+
+          <div className="navbar-username text-center">
+            {/* {this.props.name} */}
+            {this.capitalize(this.props.name)}
+          </div>
+
+          <div className="navbar-userinfo text-center">
+            <span className="navbar-org">{this.props.org}</span>
+            <span className="navbar-code"># {this.props.code}</span>
+          </div>
+        </div>
+
+        <div className="navbar-logout-area" onClick={this.logout}>
+          <div className="navbar-logout text-center">Logout</div>
+        </div>
       </div>
     );
   }
@@ -31,17 +55,18 @@ class Navbar extends Component {
 
 // this brings in the state to display on this component
 const mapStateToProps = state => {
-  // console.log(state)
+  console.log(state);
   return {
-    userId: state.userId
-
+    userId: state.userId,
+    name: state.name,
+    org: state.org.toUpperCase(),
+    code: state.code
   };
 };
 
 const mapDispachToProps = dispach => {
   return {
-    logoutUser: (userId) => {
-   
+    logoutUser: userId => {
       dispach({
         type: "LOG_OUT",
         payload: { userId }
