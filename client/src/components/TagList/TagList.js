9eb35f6fe2189ad79e83c119b1e2f6615e67695c
bjsmith3185 <bjsmith3185@hotmail.com>
2019-05-04 19:24:07 -0400

    finished MultiResults component

--- client/src/components/TagList/TagList.js
+++ client/src/components/TagList/TagList.js
@@ -1,42 +1,67 @@
 import React from "react";
 import "./TagList.css";
 
-function capitalize (string) {
+function capitalize(string) {
   return string
     .toLowerCase()
     .split(" ")
     .map(s => s.charAt(0).toUpperCase() + s.substring(1))
     .join(" ");
-};
+}
 
 const TagList = props => (
-  <div className="tags-area">
+  <div className="tag-area">
     {/* <div className="tags-heading text-center">Results, Select one to continue</div> */}
     {props.tags.map((item, index) => (
-      <div 
-      key={item._id} 
-      className="tag-occurance"
-      onClick={() => props.selectTag(index)}>
-
-        <div className="tag-state">State: 
-        <span className="tag-state-var"> {item.state.toUpperCase()}</span> 
+      <div
+        key={item._id}
+        className="tag-occurance"
+        onClick={() => props.selectTag(index)}
+      >
+        <div className="tag-state">
+          State:
+          <span className="tag-state-var"> {item.state.toUpperCase()}</span>
         </div>
 
         <div className="tag-make-model-area">
-          <div className="tag-make">Make: 
-            <span className="tag-make-var"> {capitalize(item.vehicleMake)}</span>
+          <div className="tag-make">
+            Make:
+            <span className="tag-make-var">
+              {" "}
+              {capitalize(item.vehicleMake)}
+            </span>
           </div>
-          <div className="tag-model">Model: 
-            <span className="tag-model-var"> {capitalize(item.vehicleModel)}</span>
+          <div className="tag-model">
+            Model:
+            <span className="tag-model-var">
+              {" "}
+              {capitalize(item.vehicleModel)}
+            </span>
           </div>
         </div>
-        
-        <div className="tag-year">Year: {item.vehicleYear} </div>
-        <div className="tag-color">Color: {item.vehicleColor} </div>
-        <div className="tag-owner">Owner: {item.owner} </div>
-        
-        <div className="multi-result-info">(Click to continue)</div>
-        <hr/>
+
+        <div className="tag-year-color-area">
+          <div className="tag-year">
+            Year:
+            <span className="tag-year-var"> {item.vehicleYear}</span>
+          </div>
+          <div className="tag-color">
+            Color:
+            <span className="tag-color-var">
+              {" "}
+              {capitalize(item.vehicleColor)}
+            </span>
+          </div>
+        </div>
+
+        <div className="tag-owner-area">
+          <div className="tag-owner">
+            Owner:
+            <span className="tag-owner-var"> {capitalize(item.owner)}</span>
+          </div>
+        </div>
+
+        <div className="multi-result-info text-center">(Click to continue)</div>
       </div>
     ))}
   </div>
@@ -49,11 +74,3 @@ export default TagList;
 // vehicleYear
 // owner
 // state
-
-
-
-
-
-
-
-
